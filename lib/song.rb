f65c82dd8ec9a1b24bda18a52d1a0c703df2c83b
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-10-04 02:06:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,58 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    self.all << song
+    song
+  end
+
+  def self.new_by_name(title)
+    song = self.new
+    song.name = title
+    song
+  end
+
+  def self.create_by_name(title)
+    song = self.create_by_name
+    song.name = title
+    song
+  end
+
+  def self.find_by_name(song_name)
+    @@songs.find{|s| s.name ==  song_name}
+    @@songs.find do |s|
+      if s.name == song_name
+        puts song_name
+      else
+        false
+      end
+    end
+  end
+
+  def self.find_or_create_by_name
+
+  end
+
+  def alphabetical
+    sorted = self.all.sort_by {|song| song.name}
+    sorted
+  end
+
+  def self.new_from_filename(filename)
+    song_array = filename.split(" - ")
+    song_array[1] = song_array[1].chomp(".mp3")
+    song = self.new_from_filename
+    song.name = song_array[1]
+    song.artist_name = song_array[0]
+    song
+  end
+
+  def self.create_from_filename
+
+  end
+
+  def destroy_all
+    self.class.clear_all
+  end
 end
